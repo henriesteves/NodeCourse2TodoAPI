51ccbafb889b308b23057730e095d38edb1ba007
Henrique Esteves <henriesteves@gmail.com>
2018-09-01 12:05:21 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,19 +12,37 @@ const port = 3000
 
 app.use(bodyParser.json())
 
+// POST /todos
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
   })
 
   todo.save()
-    .then((doc) => {
+    .then(doc => {
       res.send(doc)
     }, e => {
       res.status(400)
       res.send(e)
     })
 })
+
+// GET /todos
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos })
+    }, e => {
+      res.send(400)
+      res.send(e)
+    })
+})
+
+// GET /todos/:id
+
+// DELETE /todos/:id
+
+// PATCH /todos/:id
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
